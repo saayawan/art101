c76af36fe701b03a9565c9563db6b9cf47f48db8
Saaya <saayawan@icloud.com>
2025-05-19 23:21:59 -0700

    fix

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -27,61 +27,54 @@ $("#submit").click(function(){
 });
 */
 
+$(document).ready(function() {
 
-// Function to sort a string alphabetically
-function sortString(inputString) {
-  return inputString.split('').sort().join('');
-}
-
-// Function to shuffle string for an anagram
-function anagram(inputString) {
-  const arr = inputString.split('');
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+  function sortString(inputString) {
+    return inputString.split('').sort().join('');
   }
-  return arr.join('');
-}
 
-// Click listener for "Submit" button (sorts name)
-$("#submit").click(function(){
-  const userName = $("#user-name").val();
-  const sortedName = sortString(userName);
-  $("#output").append(`<p><strong>Sorted:</strong> ${sortedName}</p>`);
+  function anagram(inputString) {
+    const arr = inputString.split('');
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr.join('');
+  }
+
+  $("#submit").click(function(){
+    const userName = $("#user-name").val();
+    const sortedName = sortString(userName);
+    $("#output").append(`<p><strong>Sorted:</strong> ${sortedName}</p>`);
+  });
+
+  $("#anagram").click(function(){
+    const userName = $("#user-name").val();
+    const mixedName = anagram(userName);
+    $("#output").append(`<p><strong>Anagram:</strong> ${mixedName}</p>`);
+  });
+
+  $("#glitch").click(function(){
+    $("body").css("background-color", "black");
+    $("*").css("color", "lime");
+    $("#output").append("<p>⚠️ Glitch Activated ⚠️</p>");
+    setTimeout(() => {
+      $("body").css("background-color", "");
+      $("*").css("color", "");
+    }, 2000);
+  });
+
+  $("#dark-mode").click(function(){
+    $("body").toggleClass("dark-mode");
+  });
+
+  setInterval(() => {
+    $("body").css("background-color", "black");
+    $("*").css("color", "magenta");
+    $("#output").append("<p><em>The matrix pulses... </em></p>");
+    setTimeout(() => {
+      $("body").css("background-color", "");
+      $("*").css("color", "");
+    }, 1000);
+  }, 10000);
 });
-
-// Anagram button
-$("#anagram").click(function(){
-  const userName = $("#user-name").val();
-  const mixedName = anagram(userName);
-  $("#output").append(`<p><strong>Anagram:</strong> ${mixedName}</p>`);
-});
-
-// Glitch button - inverts colors temporarily
-$("#glitch").click(function(){
-  $("body").css("background-color", "black");
-  $("*").css("color", "lime");
-  $("#output").append("<p>⚠️ Glitch Activated ⚠️</p>");
-  setTimeout(() => {
-    $("body").css("background-color", "");
-    $("*").css("color", "");
-  }, 2000);
-});
-
-// Dark mode toggle (just for fun)
-$("#dark-mode").click(function(){
-  $("body").toggleClass("dark-mode");
-});
-
-// Auto-glitch every 10 seconds
-setInterval(() => {
-  $("body").css("background-color", "black");
-  $("*").css("color", "magenta");
-  $("#output").append("<p><em>The matrix pulses... </em></p>");
-  setTimeout(() => {
-    $("body").css("background-color", "");
-    $("*").css("color", "");
-  }, 1000);
-}, 10000);
-
-
