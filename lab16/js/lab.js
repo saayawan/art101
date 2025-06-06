d19a238c7c65e98d8638c5cf03dca67a802fae23
Saaya <saayawan@icloud.com>
2025-06-05 18:59:10 -0700

    fix

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,44 +3,46 @@
 // Date: 2025-06-05
 
 function getAndDisplayComic(comicNum = "") {
-  let url = comicNum ? `https://xkcd.com/${comicNum}/info.0.json` : `https://xkcd.com/info.0.json`;
+  let xkcdUrl = comicNum
+    ? `https://xkcd.com/${comicNum}/info.0.json`
+    : `https://xkcd.com/info.0.json`;
+
+  let proxyUrl = `https://api.allorigins.win/raw?url=${encodeURIComponent(xkcdUrl)}`;
 
   $.ajax({
-    url: url,
+    url: proxyUrl,
     type: "GET",
     dataType: "json",
-    success: function(comicObj) {
-      console.log(comicObj);
+    success: function (comicObj) {
+      console.log("Comic Data:", comicObj);
 
-      // Build HTML output
       let title = `<h3>${comicObj.title}</h3>`;
       let image = `<img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}" width="600">`;
       let altText = `<p><em>${comicObj.alt}</em></p>`;
-      let output = title + image + altText;
+      let outputHTML = title + image + altText;
 
-      // Update the page
-      $("#output").html(output);
+      $("#output").html(outputHTML);
+
       $("#output").data("comicNum", comicObj.num);
     },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.log("Error:", textStatus, errorThrown);
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error("Error:", textStatus, errorThrown);
+      $("#output").html("<p>Failed to load comic. Try again later.</p>");
     }
   });
 }
 
-$(document).ready(function() {
-  // Load the latest comic initially
+$(document).ready(function () {
   getAndDisplayComic();
 
-  // Button handlers
-  $("#prev").click(function() {
+  $("#prev").click(function () {
     let current = $("#output").data("comicNum");
     if (current > 1) {
       getAndDisplayComic(current - 1);
     }
   });
 
-  $("#next").click(function() {
+  $("#next").click(function () {
     let current = $("#output").data("comicNum");
     getAndDisplayComic(current + 1);
   });
