db2f4df0df8734bd576f6bdbb345d72bcb0daf49
Saaya <saayawan@icloud.com>
2025-06-05 19:05:03 -0700

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,7 +2,7 @@
 // Author: Saaya Wan
 // Date: 2025-06-05
 
-function getAndDisplayComic(comicNum = "") {
+/*function getAndDisplayComic(comicNum = "") {
   let xkcdUrl = comicNum
     ? `https://xkcd.com/${comicNum}/info.0.json`
     : `https://xkcd.com/info.0.json`;
@@ -47,3 +47,64 @@ $(document).ready(function () {
     getAndDisplayComic(current + 1);
   });
 });
+//*/
+
+// bonus! Store latest comic number globally
+let latestComic = null;
+
+function getAndDisplayComic(comicNum = "") {
+  let xkcdUrl = comicNum
+    ? `https://xkcd.com/${comicNum}/info.0.json`
+    : `https://xkcd.com/info.0.json`;
+
+  let proxyUrl = `https://api.allorigins.win/raw?url=${encodeURIComponent(xkcdUrl)}`;
+
+  $.ajax({
+    url: proxyUrl,
+    type: "GET",
+    dataType: "json",
+    success: function (comicObj) {
+      console.log("Comic Data:", comicObj);
+
+      let title = `<h3>${comicObj.title}</h3>`;
+      let image = `<img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}" width="600">`;
+      let altText = `<p><em>${comicObj.alt}</em></p>`;
+      let outputHTML = title + image + altText;
+
+      $("#output").html(outputHTML);
+      $("#output").data("comicNum", comicObj.num);
+
+      // If we're viewing the latest comic, store its number
+      if (!latestComic) {
+        latestComic = comicObj.num;
+      }
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error("Error:", textStatus, errorThrown);
+      $("#output").html("<p>Failed to load comic. Try again later.</p>");
+    }
+  });
+}
+
+$(document).ready(function () {
+  // Load the latest comic
+  getAndDisplayComic();
+
+  // Prev button
+  $("#prev").click(function () {
+    let current = $("#output").data("comicNum");
+    if (current > 1) {
+      getAndDisplayComic(current - 1);
+    }
+  });
+
+  // Next button
+  $("#next").click(function () {
+    let current = $("#output").data("comicNum");
+    if (latestComic && current < latestComic) {
+      getAndDisplayComic(current + 1);
+    }
+  });
+});
+
+
